29306d036c170afd29e1eab4df66a9fd0b81e78a
Alexander Booth <abooth@login24.chn.perlmutter.nersc.gov>
2024-09-01 23:40:16 -0700

    Update tag in install_tms_reco.

--- nersc/run-tms-reco/install_tms_reco.sh
+++ nersc/run-tms-reco/install_tms_reco.sh
@@ -6,8 +6,7 @@ rm -rf dune-tms
 
 ( git clone https://github.com/DUNE/dune-tms.git
   cd dune-tms || exit
-  # Tag from Jeff K.
-  git checkout tags/TMS_Reco_v0.2 
+  git checkout tags/nd-production-v01.00
   source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh 
   setup edepsim v3_2_0 -f Linux64bit+3.10-2.17 -q e20:prof 
   make )
