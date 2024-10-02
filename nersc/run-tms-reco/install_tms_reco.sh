6a8982d300b86dc363d9923516a7955ee41d5870
alexbooth92 <a.c.booth92@gmail.com>
2024-10-02 04:05:57 -0700

    Update TMS tag in install script.

--- nersc/run-tms-reco/install_tms_reco.sh
+++ nersc/run-tms-reco/install_tms_reco.sh
@@ -6,7 +6,7 @@ rm -rf dune-tms
 
 ( git clone https://github.com/DUNE/dune-tms.git
   cd dune-tms || exit
-  git checkout tags/nd-production-v01.00
+  git checkout tags/0.3.0
   source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh 
   setup edepsim v3_2_0 -f Linux64bit+3.10-2.17 -q e20:prof 
   make )
