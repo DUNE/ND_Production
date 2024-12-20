5ef32e1994be4dd6128c50267766afa6c58a5864
jdkio <68720233+jdkio@users.noreply.github.com>
2024-12-20 10:28:22 -0500

    Don't want to use lower()

--- scripts/ProcessND.py
+++ scripts/ProcessND.py
@@ -350,7 +350,7 @@ if __name__ == "__main__":
       else:
           all_files=[]
           for root, dirnames, filenames in os.walk(args.indir):
-            for filename in fnmatch.filter(map(lambda x: x.lower(), filenames), '*.edep*.root'):
+            for filename in fnmatch.filter(map(lambda x: x, filenames), '*.root'):
               fullfilename = os.path.join(root, filename)
               all_files.append(fullfilename)
           N_TO_SHOW = len(all_files)
