3cc4e1fe3afa6d8b38db166a69d8ba576a025e35
Matt Kramer <mjkramer@users.noreply.github.com>
2025-02-11 16:16:19 -0800

    Default ARCUBE_RUNTIME to NONE
    Allows for simplifying of some run scripts

--- util/reload_in_container.inc.sh
+++ util/reload_in_container.inc.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-# NOTE: We're not currently running larnd-sim in a container (at least on
-# NERSC). The actual CUDA version used for larnd-sim is specified in
-# run_larnd_sim.sh etc.
-setup_cuda() {
-    module unload cudatoolkit 2>/dev/null
-    module load cudatoolkit/12.2
-}
-
-# Assume Shifter if ARCUBE_RUNTIME is unset.
-# (Individual scripts can override this; e.g. larnd-sim by default runs on the
-# host, not in Shifter)
-export ARCUBE_RUNTIME=${ARCUBE_RUNTIME:-SHIFTER}
+export ARCUBE_RUNTIME=${ARCUBE_RUNTIME:-NONE}
 
 if [[ "$ARCUBE_RUNTIME" == "SHIFTER" ]]; then
     # Reload in Shifter
