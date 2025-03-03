1140af05009d07f6458314281079472e360034b5
giacomo-santoni <giacomo.santoni12@gmail.com>
2025-03-03 09:58:38 +0100

    with SAND xsec files and SAND integration of edep-sim

--- setup-edepsim.sh
+++ setup-edepsim.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export BASE_DIR="/storage/gpfs_data/neutrino/users/gsantoni"
 export ND_PRODUCTION="$BASE_DIR/ND_Production"
 
@@ -7,18 +9,18 @@ cd $PWD
 
 # nuove di edepsim
 export ARCUBE_CONTAINER=sim2x2_ndlar011.sif
-export ARCUBE_GENIE_NAME=genie-arcube-events
-export ARCUBE_EDEP_MAC=macros/2x2_beam.mac
-export ARCUBE_GEOM_EDEP=geometry/Merged2x2MINERvA_v4/Merged2x2MINERvA_v4_noRock.gdml
+export ARCUBE_GENIE_NAME=genie-sand-events
+export ARCUBE_EDEP_MAC=macros/macro.mac
+export ARCUBE_GEOM_EDEP=geometry-sand/EC_yoke_corrected_1212_dev_SAND_complete_opt3_DRIFT1.gdml
 export ARCUBE_RUN_OFFSET=0
-export ARCUBE_OUT_NAME=edep-arcube-events
+export ARCUBE_OUT_NAME=edep-sand-events
 
 # stesse di genie da reimportare (o comunque capire come sistemarle)
 export ARCUBE_RUNTIME=APPTAINER
 export ARCUBE_CONTAINER_DIR="/storage/gpfs_data/neutrino/users/gsantoni/ND_Production/containers"
 export ARCUBE_DIR=$ND_PRODUCTION
-export ARCUBE_OUTDIR_BASE=$ND_PRODUCTION/productions
-export ARCUBE_LOGDIR_BASE=$ND_PRODUCTION/log
+export ARCUBE_OUTDIR_BASE=$ND_PRODUCTION/productions-volSAND
+export ARCUBE_LOGDIR_BASE=$ND_PRODUCTION/log-volSAND
 
 # for i in $(seq 0 9); do
 #     ARCUBE_INDEX=$i ./run_edep_sim.sh &
