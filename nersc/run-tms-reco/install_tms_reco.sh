5e369d38fbee03ee2442bb33b8758b5f21c41c1e
Michael Dolce <49890657+mdolce8@users.noreply.github.com>
2025-03-28 14:35:42 -0400

    Update install_tms_reco.sh
    by request, add a line to make it extra clear of the printout

--- nersc/run-tms-reco/install_tms_reco.sh
+++ nersc/run-tms-reco/install_tms_reco.sh
@@ -12,6 +12,7 @@ TAG="0.3.0"
   source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
   setup edepsim v3_2_0 -f Linux64bit+3.10-2.17 -q e20:prof
   make
+  echo '---------------------------'
   echo "Installed 'dune-tms' with git tag: $TAG" )
 
 
