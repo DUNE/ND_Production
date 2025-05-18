479980406665a5c878a2eeb934b98fffb7bb6e2e
Matt Kramer <mjkramer@users.noreply.github.com>
2025-05-17 22:28:54 -0700

    Add light support to run_ndlar_flow_ndlar.sh

--- run-ndlar-flow/run_ndlar_flow_ndlar.sh
+++ run-ndlar-flow/run_ndlar_flow_ndlar.sh
@@ -36,6 +36,13 @@ workflow3='yamls/ndlar_flow/workflows/combined/combined_reconstruction_mc.yaml'
 workflow4='yamls/ndlar_flow/workflows/charge/prompt_calibration_mc.yaml'
 workflow5='yamls/ndlar_flow/workflows/charge/final_calibration_mc.yaml'
 
+# light workflows
+workflow6='yamls/ndlar_flow/workflows/light/light_event_building_mc.yaml'
+workflow7='yamls/ndlar_flow/workflows/light/light_event_reconstruction_mc.yaml'
+
+# charge-light trigger matching
+workflow8='yamls/ndlar_flow/workflows/charge/charge_light_assoc_mc.yaml'
+
 cd "$ND_PRODUCTION_INSTALL_DIR"/ndlar_flow
 
 # Ensure that the second h5flow doesn't run if the first one crashes. This also
@@ -46,6 +53,14 @@ set -o errexit
 #run h5flow -c $workflow1 $workflow2 $workflow3 $workflow4 $workflow5\
 run h5flow -c $workflow1 $workflow2 $workflow3 $workflow4\
     -i "$inFile" -o "$outFile" $compression
+
+if [[ "$ND_PRODUCTION_CHARGE_ONLY" != "1" ]]; then
+    run h5flow -c $workflow6 $workflow7\
+        -i "$inFile" -o "$outFile" $compression
+
+    run h5flow -c $workflow8\
+        -i "$outFile" -o "$outFile" $compression
+fi
 
 mkdir -p "$outDir/FLOW/$subDir"
 mv "$outFile" "$outDir/FLOW/$subDir"
