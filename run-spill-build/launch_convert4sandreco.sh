044a2d82bf1f06ca2b6ed5ab1e406f4b72a0136d
giacomo-santoni <giacomo.santoni12@gmail.com>
2025-06-17 11:09:11 +0200

    created run-convert4sandreco folder

--- run-spill-build/launch_convert4sandreco.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export ND_PRODUCTION_DIR="/storage/gpfs_data/neutrino/users/gsantoni/ND_Production"
-export ND_PRODUCTION_RUNTIME="SINGULARITY"
-export ND_PRODUCTION_CONTAINER_DIR="/storage/gpfs_data/neutrino/users/gsantoni/ND_Production/containers"
-export ND_PRODUCTION_CONTAINER="sim2x2_ndlar011.sif"
-
-./run-convert4sandreco.sh "$@"
