2c20f9726a65de357400e8013720024585e35d5a
giacomo-santoni <giacomo.santoni12@gmail.com>
2025-07-17 12:53:29 +0200

    Conversion to user coord syst works, need to be generalized

--- run-genie/convert2User.cpp
+++ run-genie/convert2User.cpp
@@ -4,6 +4,11 @@
 
 // Beam2UserNuOrigin_dk2nu
 
+std::ostream& operator<<(std::ostream& os, const TLorentzVector& v){
+    os << "( " << v.X() << ", " << v.Y() << ", " << v.Z() << ", " << v.T() << ")";
+    return os; 
+}
+
 TLorentzVector convert2User(std::string ghep_fname){
 
     bsim::Dk2Nu* dk2nu = nullptr;
@@ -16,59 +21,69 @@ TLorentzVector convert2User(std::string ghep_fname){
     std::unique_ptr<TTree> gtree(ghep_file->Get<TTree>("gtree"));
     gtree->SetBranchAddress("dk2nu", &dk2nu);
 
-    std::vector<double> nuOriginBeamZ; 
+    // std::vector<double> nuOriginBeamX; 
+    // std::vector<double> nuOriginBeamY; 
+    // std::vector<double> nuOriginBeamZ; 
+    // std::vector<double> nuOriginBeamT;
+    double nuOriginBeamX;
+    double nuOriginBeamY;
+    double nuOriginBeamZ;
+    double nuOriginBeamT;
+
+    std::vector<TLorentzVector> nuOriginBeam;
 
     std::cout<<"entries: "<<gtree->GetEntries()<<std::endl;
 
-    for (int i = 0; i < gtree->GetEntries(); i++){
+    for (int i = 0; i < 1; i++){ // gtree->GetEntries()
+        
         gtree->GetEntry(i);
         auto ancestor = dk2nu->ancestor;
-        std::cout<<"ancestor: "<<dk2nu->ancestor[ancestor.size()-1].startz<<std::endl;
-        nuOriginBeamZ.push_back(ancestor[ancestor.size()-1].startz);
-        std::cout<<" z origin in beam coord: "<<nuOriginBeamZ[i]<<std::endl;
+        // some are outside the decay pipe (z > 221 m), but they are nu produced by muon decay
+        // std::cout<<" z origin in beam coord: "<<nuOriginBeamZ[i]<<std::endl;
+        // std::cout<<" t origin in beam coord: "<<nuOriginBeamT[i]<<std::endl;
+        nuOriginBeamX = ancestor[ancestor.size()-1].startx;
+        nuOriginBeamY = ancestor[ancestor.size()-1].starty;
+        nuOriginBeamZ = ancestor[ancestor.size()-1].startz;
+        nuOriginBeamT = ancestor[ancestor.size()-1].startt;
+
+        TLorentzVector nuOriginBeam_tmp(nuOriginBeamX, nuOriginBeamY, nuOriginBeamZ, nuOriginBeamT);
+        nuOriginBeam.push_back(nuOriginBeam_tmp);
+
+        std::cout<<"nuOriginBeam "<<nuOriginBeam[i]<<std::endl;
     }
 
-    // TLorentzVector nuOriginBeam(nuOriginBeamX[0], nuOriginBeamY[0], nuOriginBeamZ[0], nuOriginBeamT[0]);
     TLorentzVector nuOriginUser;
 
-    // // define rotation
-    // TRotation r;
-    // double a = -0.101; //rad
-    // TLorentzRotation fBeamRot = r.RotateX(a);
+    // define rotation
+    TRotation r;
+    double a = -0.101; // rad
+    TRotation fBeamRot = r.RotateX(a);
 
-    // // define fBeamZero
-    // TVector3 userpos(0.0, 0.05387, 6.66); //m
-    // TVector3 beampos(0, 0, 562.1179); //m
-    // TVector3 beam0 = userpos - fBeamRot*beampos;
-    // TLorentzVector fBeamZero(beam0, 0);
+    // define fBeamZero
+    TVector3 userpos(0.0, 0.05387, 6.66); // m
+    TVector3 beampos(0, 0, 562.1179); // m
+    TVector3 beam0 = userpos - fBeamRot*beampos;
+    TLorentzVector fBeamZero(beam0, 0);
 
-    // double fLengthScaleB2U = 1/100;
+    // conversion factors
+    double fLengthScaleB2U = 1./100.;
+    double fLengthTimeB2U = 1./1E9;
 
-    // nuOriginUser = fLengthScaleB2U*(fBeamRot*nuOriginBeam) + fBeamZero;
+    std::cout<<"nuOriginBeam "<<nuOriginBeam[0]<<std::endl;
+    nuOriginUser = fLengthScaleB2U*(TLorentzRotation(fBeamRot)*nuOriginBeam[0]) + fBeamZero;
+    nuOriginUser.SetT(nuOriginBeam[0].T()*fLengthTimeB2U);
+    std::cout<<"nuOriginUser "<<nuOriginUser<<std::endl;
+
+    
+    // check
+    // double const c = 29.9792458; // cm/ns
+    // double time = 56211.79/c; // ns
+    // TLorentzVector NDHallBeam(0, 0, 56211.79, time);
+    // TLorentzVector NDHallUser;
+    // std::cout<<"NDHallBeam "<<NDHallBeam<<std::endl;
+    // NDHallUser = fLengthScaleB2U*(TLorentzRotation(fBeamRot)*NDHallBeam) + fBeamZero;
+    // NDHallUser.SetT(NDHallBeam.T()*fLengthTimeB2U);
+    // std::cout<<"NDHallUser "<<NDHallUser<<std::endl;
     
     return nuOriginUser;
 }
-
-// void GDk2NuFlux::Beam2UserPos(const TLorentzVector& beamxyz,
-//                                    TLorentzVector& usrxyz) const
-// {
-//   usrxyz = fLengthScaleB2U*(fBeamRot*beamxyz) + fBeamZero;
-//   // above assumed T=0
-//   usrxyz.SetT(beamxyz.T()*fTimeScaleB2U);
-// }
-
-// ROOT::RDataFrame df("gtree", ghep_fname);
-// auto df2 = df.Define("nu_ancestor_startx", 
-// [](const std::vector<double>& v) { return v.back(); }, 
-// {"ancestor.startx"}).Define("nu_ancestor_starty", 
-// [](const std::vector<double>& v) { return v.back(); }, 
-// {"ancestor.starty"}).Define("nu_ancestor_startz", 
-// [](const std::vector<double>& v) { return v.back(); }, 
-// {"ancestor.startz"}).Define("nu_ancestor_startt", 
-// [](const std::vector<double>& v) { return v.back(); }, 
-// {"ancestor.startt"});
-
-// auto nuOriginBeamX = df.Take<Double_t>("ancestor.startx");
-// auto nuOriginBeamY = df.Take<Double_t>("ancestor.starty");
-// auto nuOriginBeamZ = df.Take<Double_t>("ancestor.startz");
-// auto nuOriginBeamT = df.Take<Double_t>("ancestor.startt");
--- run-genie/run_convert2User.sh
+++ run-genie/run_convert2User.sh
@@ -3,7 +3,7 @@
 source ../util/reload_in_container.inc.sh
 source ../util/init.inc.sh
 
-ghepFile="../productions-SAND_opt3_DRIFT1/run-genie/sand-events/GHEP/0000000/sand-events.0000000.GHEP.root"
+ghepFile="../productions-new-flux/run-genie/sand-events/GHEP/0000000/sand-events.0000000.GHEP.root"
 
 LIBTG4EVENT_DIR=${LIBTG4EVENT_DIR:-libTG4Event}
 # LD_PRELOAD="/usr/lib64/libxml2.so:/opt/generators/genie/lib/libGFwUtl-3.04.00.so:/opt/generators/root/lib/libHist.so:/opt/generators/root/lib/libPhysics.so" /opt/generators/root/bin/root.exe
