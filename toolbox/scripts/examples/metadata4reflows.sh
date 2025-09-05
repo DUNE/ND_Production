9062d5ceba996343bb4e7978240ed8b94638fac6
Matt Kramer <mjkramer@users.noreply.github.com>
2025-09-05 13:31:13 -0700

    Update metadata4reflows.sh example

--- toolbox/scripts/examples/metadata4reflows.sh
+++ toolbox/scripts/examples/metadata4reflows.sh
@@ -2,6 +2,7 @@
 
 # Examples of generating metadata for reflows
 
+
 # 2x2 v11:
 
 base_2x2=/global/cfs/cdirs/dune/www/data/2x2/reflows/v11
@@ -11,6 +12,12 @@ python DumpAllMetadata.py --stage flow --release v1.5.0 --dir $base_2x2/flow --f
 
 python DumpAllMetadata.py --stage pandora --release v01-02-02 --dir $base_2x2/pandora --filelist $filelist_2x2 --config proto_nd --replace --parallel 10
 
+SPINE_SETTINGS=2x2_full_chain_data_240819.cfg python DumpAllMetadata.py --stage spine --release 6a44ceffe56 --dir $base_2x2/spine --filelist $filelist_2x2 --config proto_nd --replace --parallel 10
+
+python DumpAllMetadata.py --stage caf --release 8ad48f2 --dir $base_2x2/caf --filelist $filelist_2x2 --config proto_nd --replace --parallel 10
+
+python DumpAllMetadata.py --stage caf_flat --release 8ad48f2 --dir $base_2x2/caf.flat --filelist $filelist_2x2 --config proto_nd --replace --parallel 10
+
 
 # FSD v5:
 
@@ -18,3 +25,11 @@ base_fsd=/global/cfs/cdirs/dune/www/data/FSD/reflows/v5
 filelist_fsd=/pscratch/sd/d/dunepro/mkramer/install/Reflow_FSD_v5/inputs.fsd.everything.json
 
 python DumpAllMetadata.py --stage flow --release v1.5.1p1 --dir $base_fsd/flow --filelist $filelist_fsd --config fsd --replace --parallel 10
+
+python DumpAllMetadata.py --stage pandora --release v01-02-02 --dir $base_fsd/pandora --filelist $filelist_fsd --config fsd --replace --parallel 10
+
+SPINE_SETTINGS=ndlar_single_full_chain_data_250629.cfg python DumpAllMetadata.py --stage spine --release 08b2fc3 --dir $base_fsd/spine --filelist $filelist_fsd --config fsd --replace --parallel 10
+
+python DumpAllMetadata.py --stage caf --release e17772a --dir $base_fsd/caf --filelist $filelist_fsd --config fsd --replace --parallel 10
+
+python DumpAllMetadata.py --stage caf_flat --release e17772a --dir $base_fsd/caf.flat --filelist $filelist_fsd --config fsd --replace --parallel 10
