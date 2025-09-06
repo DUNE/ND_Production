1d77874f3c3aabcf51937e141cf4a5f44e2d783c
Matt Kramer <mjkramer@users.noreply.github.com>
2025-09-05 17:03:49 -0700

    f

--- toolbox/scripts/examples/metadata4reflows.sh
+++ toolbox/scripts/examples/metadata4reflows.sh
@@ -5,8 +5,10 @@
 
 # 2x2 v11:
 
-base_2x2=/global/cfs/cdirs/dune/www/data/2x2/reflows/v11
-filelist_2x2=/pscratch/sd/d/dunepro/mkramer/install/Reflow_2x2_v11/inputs.2x2.everything.json
+# base_2x2=/global/cfs/cdirs/dune/www/data/2x2/reflows/v11
+base_2x2=/global/cfs/cdirs/m3249/dune/matt-test/2x2
+# filelist_2x2=/pscratch/sd/d/dunepro/mkramer/install/Reflow_2x2_v11/inputs.2x2.everything.json
+filelist_2x2=/pscratch/sd/d/dunepro/mkramer/install/Reflow_2x2_v11/inputs.2x2.raw_minerva.json
 
 python DumpAllMetadata.py --stage flow --release v1.5.0 --dir $base_2x2/flow --filelist $filelist_2x2 --config proto_nd --replace --parallel 10
 
@@ -21,7 +23,8 @@ python DumpAllMetadata.py --stage caf_flat --release 8ad48f2 --dir $base_2x2/caf
 
 # FSD v5:
 
-base_fsd=/global/cfs/cdirs/dune/www/data/FSD/reflows/v5
+# base_fsd=/global/cfs/cdirs/dune/www/data/FSD/reflows/v5
+base_fsd=/global/cfs/cdirs/m3249/dune/matt-test/fsd
 filelist_fsd=/pscratch/sd/d/dunepro/mkramer/install/Reflow_FSD_v5/inputs.fsd.everything.json
 
 python DumpAllMetadata.py --stage flow --release v1.5.1p1 --dir $base_fsd/flow --filelist $filelist_fsd --config fsd --replace --parallel 10
