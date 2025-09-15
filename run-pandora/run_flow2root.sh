ed3c0842b2df34c7d78c57c8bc1647867fde647a
Will Foreman <wforeman.phys@gmail.com>
2025-09-15 13:08:07 -0700

    Changes to run_flow2root script needed for MiniRun6.5

--- run-pandora/run_flow2root.sh
+++ run-pandora/run_flow2root.sh
@@ -28,14 +28,20 @@ inFile=${ND_PRODUCTION_OUTDIR_BASE}/run-ndlar-flow/${ND_PRODUCTION_IN_NAME}/FLOW
 isData=1
 [ "${ND_PRODUCTION_PANDORA_INPUT_FORMAT}" ==  "SPMC" ] && isData=0
 
+# Use *prompt* hits for simulation until truth-matching
+# issues can be fully debugged. - Aug 20, 2025
+#isFinal=${ND_PRODUCTION_USE_FINAL_HITS:-1}
+isFinal=0
+
 # Convert input HDF5 file to ROOT
 source $ND_PRODUCTION_PANDORA_INSTALL/pandora.venv/bin/activate
-python3 $ND_PRODUCTION_PANDORA_INSTALL/LArRecoND/ndlarflow/h5_to_root_ndlarflow.py $inFile $isData $tmpOutDir
+python3 $ND_PRODUCTION_PANDORA_INSTALL/LArRecoND/ndlarflow/h5_to_root_ndlarflow.py $inFile $isData $isFinal ${tmpOutDir}/${inName}.tmp.root
+run root -l -q $ND_PRODUCTION_PANDORA_INSTALL/LArRecoND/ndlarflow/rootToRootConversion.C+\(true,\"${tmpOutDir}/${inName}.tmp.root\",\"${tmpOutDir}/${inName}.FLOW.hdf5_hits.root\"\)
 deactivate
 
 # Move ROOT file from tmpOutDir to output directory
 rootOutDir=$outDir/FLOW/$subDir
 mkdir -p "${rootOutDir}"
-rootFile=${rootOutDir}/${outName}.FLOW.hdf5_hits.root
 tmpRootFile=${tmpOutDir}/${inName}.FLOW.hdf5_hits.root
+rootFile=${rootOutDir}/${outName}.FLOW.hdf5_hits.root
 mv "${tmpRootFile}" "${rootFile}"
