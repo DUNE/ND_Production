7378d0e4636cd8d4709ec50c9d26086248e128e2
alexbooth92 <a.c.booth92@gmail.com>
2025-10-16 01:35:52 -0700

    Add hit merging step to ndlar flow job script.

--- run-ndlar-flow/run_ndlar_flow_ndlar.sh
+++ run-ndlar-flow/run_ndlar_flow_ndlar.sh
@@ -34,14 +34,15 @@ workflow1='yamls/ndlar_flow/workflows/charge/charge_event_building_mc.yaml'
 workflow2='yamls/ndlar_flow/workflows/charge/charge_event_reconstruction_mc.yaml'
 workflow3='yamls/ndlar_flow/workflows/combined/combined_reconstruction_mc.yaml'
 workflow4='yamls/ndlar_flow/workflows/charge/prompt_calibration_mc.yaml'
-workflow5='yamls/ndlar_flow/workflows/charge/final_calibration_mc.yaml'
+workflow5='yamls/ndlar_flow/workflows/charge/merged_calibration_mc.yaml'
+workflow6='yamls/ndlar_flow/workflows/charge/final_calibration_mc.yaml'
 
 # light workflows
-workflow6='yamls/ndlar_flow/workflows/light/light_event_building_mc.yaml'
-workflow7='yamls/ndlar_flow/workflows/light/light_event_reconstruction_mc.yaml'
+workflow7='yamls/ndlar_flow/workflows/light/light_event_building_mc.yaml'
+workflow8='yamls/ndlar_flow/workflows/light/light_event_reconstruction_mc.yaml'
 
 # charge-light trigger matching
-workflow8='yamls/ndlar_flow/workflows/charge/charge_light_assoc_mc.yaml'
+workflow9='yamls/ndlar_flow/workflows/charge/charge_light_assoc_mc.yaml'
 
 cd "$ND_PRODUCTION_INSTALL_DIR"/ndlar_flow
 
@@ -49,13 +50,13 @@ cd "$ND_PRODUCTION_INSTALL_DIR"/ndlar_flow
 # ensures that we properly report the failure to the production system.
 set -o errexit
 
-run h5flow -c $workflow1 $workflow2 $workflow3 $workflow4 $workflow5\
+run h5flow -c $workflow1 $workflow2 $workflow3 $workflow4 $workflow5 $workflow6\
     -i "$inFile" -o "$outFile" $compression
 
-run h5flow -c $workflow6 $workflow7\
+run h5flow -c $workflow7 $workflow8\
     -i "$inFile" -o "$outFile" $compression
 
-run h5flow -c $workflow8\
+run h5flow -c $workflow9\
     -i "$outFile" -o "$outFile" $compression
 
 mkdir -p "$outDir/FLOW/$subDir"
