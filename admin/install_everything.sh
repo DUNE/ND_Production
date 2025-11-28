42b184d784c7aa21234777e191d06e77a4214eb2
giacomo-santoni <giacomo.santoni12@gmail.com>
2025-11-28 10:06:25 +0100

    first draft of modifications

--- admin/install_everything.sh
+++ admin/install_everything.sh
@@ -2,17 +2,14 @@
 
 # Run me from the root directory of ND_Production
 
-if [ -z "$1" ]; then
-  echo "Installation is detector specific, you must pass either '2x2' or 'ndlar' as"
-  echo "the first positional argument of this script."
-  exit 1
-fi
-
 detector=$1
 
-pushd run-corsika
-./install_corsika.sh
-popd
+# we don't need it i think
+# pushd run-corsika
+# ./install_corsika.sh
+# popd
+
+echo "Installation of common tools"
 
 # If using the ND_PRODUCTION_USE_GHEP_POT option, need to install a single
 # executable via install_hadd.sh.
@@ -20,25 +17,29 @@ pushd run-hadd
 ./install_hadd.sh
 popd
 
-pushd run-larnd-sim
-./install_larnd_sim.sh
-popd
-
-pushd run-ndlar-flow
-./install_ndlar_flow.sh
-popd
-
-pushd run-pandora
-./install_pandora.sh "$detector"
-popd
-
-pushd run-mlreco
-./install_mlreco.sh
-popd
-
 pushd run-cafmaker
 ./install_cafmaker.sh
 popd
+
+if [ "$detector" = "ndlar" ] || [ "$detector" = "2x2" ]; then
+  echo "Installation of $detector specific tools"
+
+  pushd run-larnd-sim 
+  ./install_larnd_sim.sh
+  popd
+
+  pushd run-ndlar-flow
+  ./install_ndlar_flow.sh
+  popd
+
+  pushd run-pandora
+  ./install_pandora.sh "$detector"
+  popd
+
+  pushd run-mlreco
+  ./install_mlreco.sh
+  popd
+fi
 
 # HACK because we forgot to include GNU time in some of the containers
 TMP_BIN="tmp_bin"
--- run-hadd/install_hadd.sh
+++ run-hadd/install_hadd.sh
@@ -5,8 +5,8 @@ export ND_PRODUCTION_RUNTIME=${ND_PRODUCTION_RUNTIME:-SHIFTER}
 
 
 # Keep track of what container was set before.
-export ORG_ND_PRODUCTION_CONTAINER=$ND_PRODUCTION_CONTAINER
-export ND_PRODUCTION_CONTAINER=mjkramer/sim2x2:ndlar011
+# export ORG_ND_PRODUCTION_CONTAINER=$ND_PRODUCTION_CONTAINER
+export ND_PRODUCTION_CONTAINER=$ND_PRODUCTION_CONTAINER_HADD
 
 
 if [[ "$ND_PRODUCTION_RUNTIME" == "SHIFTER" ]]; then
@@ -15,7 +15,12 @@ if [[ "$ND_PRODUCTION_RUNTIME" == "SHIFTER" ]]; then
         shifter --image=$ND_PRODUCTION_CONTAINER --module=none -- "$0" "$@"
         exit
     fi
-
+elif [[ "$ND_PRODUCTION_RUNTIME" == "SINGULARITY" ]]; then
+    # Reload in Singularity
+    if [[ "$SINGULARITY_NAME" != "$ND_PRODUCTION_CONTAINER" ]]; then
+        singularity exec -B $ND_PRODUCTION_DIR,$ND_PRODUCTION_OUTDIR_BASE,$ND_PRODUCTION_LOGDIR_BASE $ND_PRODUCTION_CONTAINER_DIR/$ND_PRODUCTION_CONTAINER /bin/bash "$0" "$@"
+        exit
+    fi
 else
     echo "Unsupported \$ND_PRODUCTION_RUNTIME"
     exit
@@ -29,7 +34,7 @@ g++ -o getGhepPOT.exe getGhepPOT.C `root-config --cflags --glibs`
 
 
 # Put back the original container
-export ND_PRODUCTION_CONTAINER=$ORG_ND_PRODUCTION_CONTAINER
+# export ND_PRODUCTION_CONTAINER=$ORG_ND_PRODUCTION_CONTAINER
 
 
 exit
