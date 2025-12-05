5661b0377c650f018b7e93835adce43a04609fee
Matt Kramer <mjkramer@users.noreply.github.com>
2025-12-05 06:39:42 -0800

    Update git tags for FSD v7

--- run-mlreco/install_mlreco.sh
+++ run-mlreco/install_mlreco.sh
@@ -66,7 +66,7 @@ pip install .
 cd ..
 
 
-git clone -b develop https://github.com/DeepLearnPhysics/flow2supera.git
+git clone -b v4.2.1 https://github.com/DeepLearnPhysics/flow2supera.git
 ## Don't pip install because e.g. config files are expected to live near
 ## __file__
 # cd flow2supera
@@ -92,7 +92,7 @@ cd ..
 # git clone -b jw_dune_nd_lar https://github.com/chenel/lartpc_mlreco3d.git
 
 #git clone -b v2.9.5 https://github.com/DeepLearnPhysics/lartpc_mlreco3d.git
-git clone -b develop https://github.com/DeepLearnPhysics/spine.git
+git clone -b v0.7.9 https://github.com/DeepLearnPhysics/spine.git
 
 # git clone https://github.com/chenel/dune-nd-lar-reco.git
 # the old yaml.load API has been removed
