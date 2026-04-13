2714d65960bc7ae645bee17c96c36b0f71aa1086
Matt Kramer <mjkramer@users.noreply.github.com>
2026-04-13 14:39:55 -0700

    Fix/restructure charge-centric reflow script

--- run-ndlar-flow/run_ndlar_flow.data.charge_centric.sh
+++ run-ndlar-flow/run_ndlar_flow.data.charge_centric.sh
@@ -14,16 +14,6 @@ outf=$tmpOutDir/$subDir/$outName.FLOW.hdf5
 mkdir -p "$tmpOutDir/$subDir"
 rm -f "$outf"
 
-if [[ -n "$ND_PRODUCTION_RUN_BINARY2PACKET" ]]; then
-    input_chargef=$tmpOutDir/$subDir/$outName.PACKET.hdf5
-    rm -f "$input_chargef"
-    # DO NOT QUOTE $ND_PRODUCTION_BINARY2PACKET_ARGS
-    run convert_rawhdf5_to_hdf5.py $ND_PRODUCTION_BINARY2PACKET_ARGS \
-        --input_filename  "$chargef" --output_filename "$input_chargef"
-else
-    input_chargef=$chargef
-fi
-
 h5flow="h5flow"
 
 if [[ "$ND_PRODUCTION_COMPRESS" != "" ]]; then
@@ -32,41 +22,47 @@ if [[ "$ND_PRODUCTION_COMPRESS" != "" ]]; then
 fi
 
 get_range() {
-    run ../../scripts/get_light_event_range.py \
-        --workflow "$workflow_light_event_build" \
+    run ../helpers/get_light_event_range.py \
+        --workflow "$ND_PRODUCTION_LIGHT_EVTBUILD_WORKFLOWS" \
         --chargef "$input_chargef" \
         --first-lightf "$(realpath "${lightfs[0]}")" \
         --last-lightf "$(realpath "${lightfs[-1]}")" \
         --tmpdir "$tmpOutDir"
 }
 
+if [[ -n "$ND_PRODUCTION_RUN_BINARY2PACKET" ]]; then
+    input_chargef=$tmpOutDir/$subDir/$outName.PACKET.hdf5
+    rm -f "$input_chargef"
+    run convert_rawhdf5_to_hdf5.py $ND_PRODUCTION_BINARY2PACKET_ARGS \
+        --input_filename  "$chargef" --output_filename "$input_chargef"
+else
+    input_chargef=$chargef
+fi
+
 if [[ -n "${lightfs[*]}" ]]; then
     read -r -a evt_range <<< "$(get_range)"
 
     for lightf in "${lightfs[@]}"; do
-       extra_args=()
-       if [[ "$lightf" == "${lightfs[0]}" ]]; then
-           extra_args+=("--start_position" "${evt_range[0]}")
-       fi
-       if [[ "$lightf" == "${lightfs[-1]}" ]]; then
-           extra_args+=("--end_position" "${evt_range[1]}")
-       fi
+        extra_args=()
+        if [[ "$lightf" == "${lightfs[0]}" ]]; then
+            extra_args+=("--start_position" "${evt_range[0]}")
+        fi
+        if [[ "$lightf" == "${lightfs[-1]}" ]]; then
+            extra_args+=("--end_position" "${evt_range[1]}")
+        fi
 
-       workflows=$ND_PRODUCTION_LIGHT_EVTBUILD_WORKFLOWS
-       # DO NOT QUOTE THE workflows ARRAY; we want it to be split
-       run $h5flow -i "$(realpath "$lightf")" -o "$outf" -c ${workflows[@]} \
-           "${extra_args[@]}"
+        workflows=$ND_PRODUCTION_LIGHT_EVTBUILD_WORKFLOWS
+        run $h5flow -i "$(realpath "$lightf")" -o "$outf" -c ${workflows[@]} \
+            "${extra_args[@]}"
     done
 
     if [[ -n "$ND_PRODUCTION_LIGHT_RECO_WORKFLOWS" ]]; then
         workflows=$ND_PRODUCTION_LIGHT_RECO_WORKFLOWS
-        # DO NOT QUOTE THE workflows ARRAY; we want it to be split
         run $h5flow -i "$outf" -o "$outf" -c ${workflows[@]}
     fi
 fi
 
 workflows="$ND_PRODUCTION_CHARGE_EVTBUILD_WORKFLOWS $ND_PRODUCTION_CHARGE_RECO_WORKFLOWS"
-# DO NOT QUOTE THE workflows ARRAY; we want it to be split
 run $h5flow -i "$input_chargef" -o "$outf" -c ${workflows[@]}
 
 if [[ -n "$ND_PRODUCTION_RUN_BINARY2PACKET" ]]; then
@@ -75,7 +71,6 @@ fi
 
 if [[ -n "${lightfs[*]}" ]]; then
     workflows=$ND_PRODUCTION_CLMATCH_WORKFLOWS
-    # DO NOT QUOTE THE workflows ARRAY; we want it to be split
     run $h5flow -i "$outf" -o "$outf" -c ${workflows[@]}
 fi
 
