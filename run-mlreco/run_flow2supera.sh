0e096d112036f76c26aadc83303920dced3fd825
Matt Kramer <mjkramer@users.noreply.github.com>
2026-05-16 17:49:27 -0700

    Set OMP_NUM_THREADS to 1 for flow2supera

--- run-mlreco/run_flow2supera.sh
+++ run-mlreco/run_flow2supera.sh
@@ -13,6 +13,8 @@ config=$ND_PRODUCTION_FLOW2SUPERA_CONFIG
 
 rm -f "$outFile"
 
+export OMP_NUM_THREADS=1
+
 run install/flow2supera/bin/run_flow2supera.py -o "$outFile" -c "$config" "$inFile"
 
 larcvOutDir=$outDir/LARCV/$subDir
