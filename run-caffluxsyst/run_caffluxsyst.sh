9a0a41527542f275b5d125db94a0f921f7690219
Alexander Booth <a.c.booth92@gmail.com>
2026-08-26 08:51:53 +0100

    Reintroduce ups setup logic in the run script after rejected internal duneanafluxtools changes.

--- run-caffluxsyst/run_caffluxsyst.sh
+++ run-caffluxsyst/run_caffluxsyst.sh
@@ -8,6 +8,12 @@ source ../util/reload_in_container.inc.sh
 setup() {
     . /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
+    # The duneanaobj version is defined by duneanafluxtools itself (its
+    # DUNE_ANAOBJ_BRANCH CMake default), not by ND_Production. Read it out of
+    # the source checkout so we don't have to duplicate/hardcode it here.
+    duneanaobj_version=$(sed -n 's/^\s*set(DUNE_ANAOBJ_BRANCH \(.*\))/\1/p' "${PWD}"/install/duneanafluxtools/CMakeLists.txt)
+    setup duneanaobj "$duneanaobj_version" -q "e20:prof"
+
     source "${PWD}"/install/duneanafluxtools/build/bin/setup.duneanafluxtools.sh
 }
 
